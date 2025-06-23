dd614d917221995b032229d42d03991bf250bcfe
Ian <ianlucasott@gmail.com>
2025-06-23 15:12:03 -0300

    archivo jar agregado

--- src/ar/edu/unlu/MSTD2025/Main.java
+++ src/ar/edu/unlu/MSTD2025/Main.java
@@ -3,7 +3,7 @@ package ar.edu.unlu.MSTD2025;
 import ar.edu.unlu.MSTD2025.Ventana.Ventana;
 
 public class Main {
-    public void main() {
+    public static void main(String[] args) {
         new Ventana();
     }
 }
